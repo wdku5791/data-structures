1c9217e1418fee0e230a18e1a222fb2698336e7c
wdku5791 <wdku5791@gmail.com>
2017-02-08 11:06:18 -0800

    passed all the stack functional instantiation pattern prompts

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,21 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var size = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[size] = value;
+    size++;
+    return value;
   };
 
   someInstance.pop = function() {
+    if (size > 0) { size--; }
+    return storage[(size)];
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
