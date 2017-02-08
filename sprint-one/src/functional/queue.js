9447e1788e19c509af2c25b43f4945c1df345e63
wdku5791 <wdku5791@gmail.com>
2017-02-08 11:41:08 -0800

    queue type functional instantiation patterns will pass

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,30 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var size = 0;
+  var front = 0;
+  var back = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[back] = value;
+    back++;
+    size++;
   };
 
   someInstance.dequeue = function() {
+    var temp = storage[(front)];
+    if (size > 0) { 
+      delete storage[front];
+      front++;
+      size--; 
+    }
+
+    return temp;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
