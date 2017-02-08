38188a9367c42f90531d9dbc9c36ea6142d60bf6
wdku5791 <wdku5791@gmail.com>
2017-02-08 12:29:38 -0800

    stack and queue type data structures pass all prompts

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.front = 0;
+  someInstance.end = 0;
+  someInstance.sizer = 0;
+  _.extend(someInstance, queueMethods);
+
+  return someInstance;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  this.storage[this.end] = value;
+  this.sizer++;
+  this.end++;
+};
+queueMethods.dequeue = function() {
+  var result = this.storage[this.front];
+  if (this.sizer > 0) {
+    delete this.storage[this.front];
+    this.sizer--;
+    this.front++;
+  }
+  return result;
+};
+queueMethods.size = function() {
+  return this.sizer;
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.sizer = 0;
+  _.extend(someInstance, stackMethods);
+
+  return someInstance;
+};
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
-var stackMethods = {};
+stackMethods = {};
+
+stackMethods.push = function(value) {
+  this.storage[this.sizer] = value;
+  this.sizer++;
+};
+stackMethods.pop = function() {
+  if (this.sizer > 0) { this.sizer--; }
+  return this.storage[(this.sizer)];
+};
+stackMethods.size = function() {
+  return this.sizer;
+};
 
 
