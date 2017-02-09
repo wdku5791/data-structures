fc6a155b179ea7a0c94ee6a886e3ab8754d335c3
wdku5791 <wdku5791@gmail.com>
2017-02-09 14:26:42 -0800

    Linkedlist will complete prompts given Author: Justin Yoo, Winston Ku

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,13 +3,31 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
+  // var headNext = 0;
+  // var counter = 0;
+
   list.addToTail = function(value) {
+    list.tail = Node(value);
+    if (list.head === null) {
+      list.head = Node(value);
+    } else {
+      list.head.next = Node(value);
+    }
+
   };
 
   list.removeHead = function() {
+    var tempHead = list.head;
+    list.head = list.head.next;
+    return tempHead.value;
   };
 
   list.contains = function(target) {
+    if (list.head.value === target || list.tail.value === target) {
+      return true;
+    } else {
+      return false;
+    }
   };
 
   return list;
