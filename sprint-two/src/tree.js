740a2ab0e930b2d72dcc083c11393d7a1e15629d
wdku5791 <wdku5791@gmail.com>
2017-02-09 16:17:33 -0800

    Tree.js completes all prompts given on tree data strucutres. Author: Justin Yoo, Winston Ku

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,21 +3,58 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
+ // _.extend(newTree.children, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+
+  if (this.value === target) {
+    return true;
+  }
+  for (var i = 0; i < this.children.length; i++) {
+    if (this.children[i].contains(target)) { return true; }
+     // return this.children[i].contains(target)
+     //return true;
+  }
+
+  return false;
+  //return result;
 };
 
+var tree = Tree();
+tree.addChild(5);
+tree.addChild(6);
+tree.children[0].addChild(7);
+tree.children[1].addChild(8);
+tree.children[0].children[0].addChild(9);
+
+tree.contains(7);
+tree.contains(5);
+tree.contains(2);
+tree.contains(9);
+tree.contains(20);
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+  // var test0 = this.value; 
+  // var test = this.children[0];
+  // var test1 = this.children[1];
+  // var result = false;
+  // if (!result) {
+  //   for (var i = 0; i < this.children.length; i++) {
+  //     if (this.children[i].value === target) {
+  //       result = true;
+  //     } 
+  //   }
+  // }
